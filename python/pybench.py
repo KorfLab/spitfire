cdba9a72e9bd2b40660f0dd44cca66220e0e3a68
iankorf <ifkorf@gmail.com>
2021-08-20 20:04:28 -0700

    tuning

--- python/pybench.py
+++ python/pybench.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
+import argparse
+import os
 import random
+import statistics
 import time
 from korflib import randomseq
 
@@ -12,13 +15,6 @@ def kmers(k, seq):
 		else: count[kmer] += 1
 	return count
 
-def countNT(seq):
-	a = seq.count('A')
-	c = seq.count('C')
-	g = seq.count('G')
-	t = seq.count('T')
-	return a, c, g, t
-
 def compare(seq1, seq2):
 	x = 0
 	for i in range(len(seq1)):
@@ -27,65 +23,73 @@ def compare(seq1, seq2):
 	return x
 
 if __name__ == '__main__':
+	
+	parser = argparse.ArgumentParser(description='pybench.py')
+	parser.add_argument('--faster', required=False, type=float, default=1.0,
+		metavar='<float>', help='go faster [%(default)f]')
+	arg = parser.parse_args()
 
-	sum = 0
-
-	## Test 1 - generate random short reads ##
-
+	speed = []
+	
+	## SEQ test
+	
 	t0 = time.time()
+	seqs = []
 	for i in range(100):
-		print(f'{i}%', end='\r')
-		for j in range(1500):
+		print(f'SEQ: {i}%', end='\r')
+		seqs = []
+		for j in range((int(1310 / arg.faster))):
 			seq = randomseq(100)
-	print()
+			seqs.append(seq)
 	t1 = time.time()
-	v = int(1000 / (t1 - t0))
-	print(f'Test #1: Speed: {v}')
-	sum += v
-
-	## Test 2 - generate random long reads and count many times ##
+	v = 100 / (t1 - t0) / arg.faster
+	speed.append(v)
+	print(f'\rSEQ: {v}')
+	
+	## I/O test
 
 	t0 = time.time()
 	for i in range(100):
-		print(f'{i}%', end='\r')
-		seq = randomseq(10000)
-		for j in range(1000):
-			countNT(seq)
-	print()
+		print(f'I/O: {i}%', end='\r')
+		for j in range(int(75 / arg.faster)):
+			seq = randomseq(100)
+			with open('tmp.pybench', 'w') as fp:
+				fp.write(seq)
+			os.system('cat tmp.pybench > /dev/null');
 	t1 = time.time()
-	v = int(1000 / (t1 - t0))
-	print(f'Test #2: Speed: {v}')
-	sum += v
+	v = 100 / (t1 - t0) / arg.faster
+	print(f'\rI/O: {v}')
+	speed.append(v)
+	
+	## MER test
 
-	## Test 3 - generate random chromosomes and count k-mers ##
+	t0 = time.time()
+	seq = randomseq(1000)
+	for i in range(100):
+		print(f'MER: {i}%', end='\r')
+		for j in range(int(480 / arg.faster)):
+			ks = kmers(15, seq)
+	t1 = time.time()
+	v = 100 / (t1 - t0) / arg.faster
+	print(f'\rMER: {v}')
+	speed.append(v)
+	
+	## CMP test
 
 	t0 = time.time()
 	for i in range(100):
-		print(f'{i}%', end='\r')
-		seq = randomseq(100000)
-		ks = kmers(15, seq)
-	print()
-	t1 = time.time()
-	v = int(1000 / (t1 - t0))
-	print(f'Test #3: Speed: {v}')
-	sum += v
-
-	## Test 4 - generate random sequences and compare them many times ##
-
-	t0 = time.time()
-	for i in range(100):
-		print(f'{i}%', end='\r')
+		print(f'CMP {i}%', end='\r')
 		s1 = randomseq(100)
 		s2 = randomseq(100)
-		for j in range(100):
+		for j in range(int(120 / arg.faster)):
 			a = compare(s1, s2)
-	print()
 	t1 = time.time()
-	v = int(1000 / (t1 - t0))
-	print(f'Test #4: Speed: {v}')
-	sum += v
+	v = 100 / (t1 - t0) / arg.faster
+	print(f'\rCMP: {v}')
+	speed.append(v)	
 
-	## Summary ##
-
-	print(f'Sum: {sum}')
+	## Summary ## should average them right?
+	for v in speed:
+		print(f'| {v:.1f}', end='')	
+	print(f' | {statistics.mean(speed):.1f}')
 
